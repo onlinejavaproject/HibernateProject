dd705967dc67214457082ce4f0d3a29ff9c7b71b
ALOK KUMAR <ak610308@gmail.com>
2021-08-15 17:39:03 +0530

    commit 1

--- HibernateProject/src/main/java/com/Get_vs_Load/FetchRecord.java
+++ HibernateProject/src/main/java/com/Get_vs_Load/FetchRecord.java
@@ -16,10 +16,10 @@ public class FetchRecord {
 		Session session = sf.openSession();
 		session.beginTransaction();
 		//Get
-		Student stu = session.get(Student.class, 6);//it will hit the database whether it is used or not
+		Student stu = session.get(Student.class, 6);//it will hit the database whether it stu used or not
 		
 		//Load
-		Student stu1 = session.load(Student.class, 8); //it will not hit database until its not used
+		Student stu1 = session.load(Student.class, 8); //it will not hit database until stu1 not used
 		System.out.print(stu1); //it will hit the database because we used object
 		
 		session.getTransaction().commit();
